a6732027a66197f89ceb7c120b2c24c931718ed3
Giacomo Dabisias <g.dabisias@sssup.it>
2016-07-12 16:17:21 +0200

    creates separate buildable modules and fixes cmake accordingly

--- cmake/ODDependency.cmake
+++ cmake/ODDependency.cmake
@@ -4,5 +4,3 @@ find_package(VTK REQUIRED)
 find_package(Eigen REQUIRED)
 find_package(Boost 1.40 COMPONENTS program_options REQUIRED)
 find_package(Glew REQUIRED)
-
-include_directories("${OD_SOURCE_DIR}" ${EIGEN_INCLUDE_DIRS} ${OpenCV_INCLUDE_DIRS} ${PCL_INCLUDE_DIRS} ${OD_SOURCE_DIR}/3rdparty/SiftGPU/src/SiftGPU)

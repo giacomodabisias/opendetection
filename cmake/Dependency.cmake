9fe86ba19a14d1952cd3f6e6e6852d2a1ed2a589
Giacomo Dabisias <g.dabisias@sssup.it>
2016-09-02 11:04:57 +0200

    removes Abhisheks work

--- cmake/Dependency.cmake
+++ cmake/Dependency.cmake
@@ -3,6 +3,7 @@ find_package(OpenCV 3 REQUIRED)
 find_package(VTK REQUIRED)
 find_package(Boost 1.40 COMPONENTS program_options REQUIRED)
 
+if(0)
 if(WITH_CAFFE)
   find_package(Caffe REQUIRED)
   if(Caffe_FOUND)
@@ -18,6 +19,7 @@ if(WITH_CAFFE)
   	message("Caffe not found!")
   endif()
 endif()
+endif(0)
 
 # Needed by od_common. There is a circular dependency between od_common and od_gpu_common include dirs.
 include_directories(GPU_COMMON_INCLUDE_DIRS ${OD_SOURCE_DIR}/gpu/common/include)
